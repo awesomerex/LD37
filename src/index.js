5b87ec0af09935307232a556eb0d629d8034e917
Rex Soriano <awesomerex@gmail.com>
2016-12-10 00:22:23 -0500

    drawing square

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log("hello");
+var PIXI = require("pixi.js");
+
+
+var renderer = PIXI.autoDetectRenderer(800, 600,{backgroundColor : 0x1099bb, view: document.getElementById('canvas')});
+
+// create the root of the scene graph
+var stage = new PIXI.Container();
+
+var container = new PIXI.Container();
+
+stage.addChild(container);
+
+var graphics = new PIXI.Graphics();
+
+// set a fill and a line style again and draw a rectangle
+graphics.lineStyle(2, 0x0000FF, 1);
+graphics.beginFill(0xFF700B, 1);
+graphics.drawRect(50, 250, 120, 120);
+graphics.endFill();
+
+stage.addChild(graphics);
+
+animate();
+
+function animate() {
+
+    renderer.render(stage);
+    requestAnimationFrame( animate );
+}
