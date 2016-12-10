51c1a80650b73e48e761f8c3faa34f1fec022513
Eric Lathrop <eric@ericlathrop.com>
2016-12-10 13:56:49 -0500

    Don't create graphics every frame

--- src/systems/graphicsFromRectangle.js
+++ src/systems/graphicsFromRectangle.js
@@ -1,16 +1,18 @@
 module.exports = function(stage){
-	return function setGraphicsFromRectangle(entities, elapsed){
-		var ids = entities.find("rectangle");
-		for ( var i = 0; i < ids.length; i++){
-			var graphics = entities.getComponent(ids[i], "graphics");
-			var rectangle = entities.getComponent(ids[i], "rectangle");
-			if(!graphics){
-				graphics = entities.addComponent(ids[i], "graphics");
-			}
-			graphics.beginFill(rectangle.color, 1);
-			graphics.drawRect(0, 0, rectangle.width, rectangle.height);
-			graphics.endFill();
-			stage.addChild(graphics);
-		}
-	}
+  return function setGraphicsFromRectangle(entities, elapsed){
+    var ids = entities.find("rectangle");
+    for ( var i = 0; i < ids.length; i++){
+      var graphics = entities.getComponent(ids[i], "graphics");
+      var rectangle = entities.getComponent(ids[i], "rectangle");
+      if (graphics) {
+        return;
+      }
+      console.log("make graphics");
+      graphics = entities.addComponent(ids[i], "graphics");
+      graphics.beginFill(rectangle.color, 1);
+      graphics.drawRect(0, 0, rectangle.width, rectangle.height);
+      graphics.endFill();
+      stage.addChild(graphics);
+    }
+  }
 }
