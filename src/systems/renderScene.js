845963ed55b2b55bbf545b10dcd82b22ef6e6b76
Eric Lathrop <eric@ericlathrop.com>
2016-12-10 21:42:48 -0500

    Fix spacing

--- src/systems/renderScene.js
+++ src/systems/renderScene.js
@@ -1,6 +1,6 @@
-module.exports = function(renderer, stage){
-	return function renderScene(entities, elapsed) {
-		var ids = entities.find("graphics");
-		renderer.render(stage);
-	}
+module.exports = function(renderer, stage) {
+  return function renderScene(entities, elapsed) {
+    var ids = entities.find("graphics");
+    renderer.render(stage);
+  }
 }
