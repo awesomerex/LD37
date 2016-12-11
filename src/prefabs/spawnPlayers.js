0e948d3ab003706a3951dd162c2c2935b6ecf64f
Eric Lathrop <eric@ericlathrop.com>
2016-12-11 18:30:22 -0500

    Intro text

--- src/prefabs/spawnPlayers.js
+++ src/prefabs/spawnPlayers.js
@@ -1,5 +1,6 @@
 var deletePlayer = require("../deletePlayer");
 var playerReady = require("./playerReady");
+var text = require("./text");
 
 module.exports = function(entities) {
   var ids = entities.find("player")
@@ -7,6 +8,8 @@ module.exports = function(entities) {
     console.log(ids, entities);
     deletePlayer(entities, ids[0]);
   }
+
+  text(entities, "2-4 Players\n\nPress gamepad\nbutton to join", 400, 300, 100, 0xffffff);
 
   playerReady(entities, 0, 100, 300, 0x00AAFF, 0, "Blue");
   playerReady(entities, 1, 700, 300, 0xFF00AA, Math.PI, "Red");
