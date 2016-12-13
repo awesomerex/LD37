a4ae6dfdabe7a5d6b7e45e3fbe6421ab1e9173ca
Eric Lathrop <eric@ericlathrop.com>
2016-12-12 19:49:17 -0500

    Reset radius

--- src/components/rectangle.js
+++ src/components/rectangle.js
@@ -3,12 +3,14 @@ module.exports = {
     return {
       width: 100,
       height: 100,
-      color: 0x0000FF
+      color: 0x0000FF,
+      radius: 0
     };
   },
   reset: function(rectangle) {
     rectangle.width = 100;
     rectangle.height = 100;
     rectangle.color = 0x0000FF;
+    rectangle.radius = 0;
   }
 }
