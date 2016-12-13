5f8d05040b4ec9dcc3e363b6bdb811efd5e3071a
Rex Soriano <awesomerex@gmail.com>
2016-12-12 23:25:43 -0500

    update press to hold

--- src/prefabs/spawnPlayers.js
+++ src/prefabs/spawnPlayers.js
@@ -8,7 +8,7 @@ module.exports = function(entities) {
     deletePlayer(entities, ids[0]);
   }
 
-  text(entities, "2-4 Players\n\nPress gamepad\nbutton to join", 400, 300, 100, 0xffffff);
+  text(entities, "2-4 Players\n\nHold gamepad\nbutton to join", 400, 300, 100, 0xffffff);
 
   playerReady(entities, 0, 100, 300, 0x00AAFF, 0, "Blue");
   playerReady(entities, 1, 700, 300, 0xFF00AA, Math.PI, "Red");
